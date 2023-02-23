3c120bb3184ea4dfde01229d46210b203c3857d0
AlaaOsama <alaaosama2121998@gmail.com>
2023-02-23 23:02:16 +0200

    commit message

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-<img src="{{asset('assets/images/logo2.jpg')}}">
-<iframe width="560" height="315" src="https://www.youtube.com/embed/_Tz4cHW500s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-{{-- @foreach ($user as $us)
-    <p>{{$us->name}}</p>
-@endforeach --}}
 
+<body>
+    <img src="{{ asset('assets/images/logo2.jpg') }}">
+    
 </body>
+
 </html>
